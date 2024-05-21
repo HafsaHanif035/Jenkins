82d209fa5edc50c886bd31d45019ea7bd58f6f22
Hafsa Hanif <113777239+HafsaHanif035@users.noreply.github.com>
2024-05-20 20:55:23 -0700

    Update program.java

--- program.java
+++ program.java
@@ -1,6 +1,6 @@
 public class program{
     public static void main(String[] args){
         System.out.println("Hello world!");
-        System.out.println("My assignment 4");
+        System.out.println("Mahnoor");
     }
 }
